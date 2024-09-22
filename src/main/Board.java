a60db1ce83af45d2f8ce0afdb6cbf7bbb90fe53d
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2024-09-22 22:31:22 +0200

    added chess pieces

--- src/main/Board.java
+++ src/main/Board.java
@@ -5,7 +5,7 @@ public class Board {
 
     final int MAX_COL = 8;
     final int MAX_ROW = 8;
-    public static final int SQUARE_SIZE = 100;
+    public static final int SQUARE_SIZE = 100; // same size of the chess pieces im using.
     public static final int HALF_SQUARE_SIZE = SQUARE_SIZE /2;
 
     public void draw(Graphics2D g2) {
